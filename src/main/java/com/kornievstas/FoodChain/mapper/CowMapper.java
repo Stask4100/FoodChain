14bfb5727e0bca804acdbc908245055e7c5df332
Stas Korniev <Stask4100@gmail.com>
2025-09-30 23:28:08 +0300

    qualifiedByName and the @Named annotation were used in the mapper

--- src/main/java/com/kornievstas/FoodChain/mapper/CowMapper.java
+++ src/main/java/com/kornievstas/FoodChain/mapper/CowMapper.java
@@ -5,6 +5,7 @@ import com.kornievstas.FoodChain.entity.Cow;
 import com.kornievstas.FoodChain.entity.Grass;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,13 +13,14 @@ import java.util.stream.Collectors;
 @Mapper(componentModel = "spring")
 public interface CowMapper {
 
-    @Mapping(target = "eatenGrassNames", expression = "java(mapGrassesToNames(cow.getEatenGrasses()))")
+    @Mapping(target = "eatenGrassNames", source = "eatenGrasses", qualifiedByName = "mapGrassesToNames")
     CowDto toDto(Cow cow);
 
     @Mapping(target = "eatenGrasses", ignore = true)
     Cow toEntity(CowDto dto);
 
-    default List<String> mapGrassesToNames(List<Grass> grasses) {
+    @Named("mapGrassesToNames")
+    static List<String> mapGrassesToNames(List<Grass> grasses) {
         if (grasses == null) return null;
         return grasses.stream()
                 .map(g -> "Grass")
--- src/main/java/com/kornievstas/FoodChain/mapper/GoatMapper.java
+++ src/main/java/com/kornievstas/FoodChain/mapper/GoatMapper.java
@@ -5,6 +5,7 @@ import com.kornievstas.FoodChain.entity.Goat;
 import com.kornievstas.FoodChain.entity.Grass;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,13 +13,14 @@ import java.util.stream.Collectors;
 @Mapper(componentModel = "spring")
 public interface GoatMapper {
 
-    @Mapping(target = "eatenGrassNames", expression = "java(mapGrassesToNames(goat.getEatenGrasses()))")
+    @Mapping(target = "eatenGrassNames", source = "eatenGrasses", qualifiedByName = "mapGrassesToNames")
     GoatDto toDto(Goat goat);
 
     @Mapping(target = "eatenGrasses", ignore = true)
     Goat toEntity(GoatDto dto);
 
-    default List<String> mapGrassesToNames(List<Grass> grasses) {
+    @Named("mapGrassesToNames")
+    static List<String> mapGrassesToNames(List<Grass> grasses) {
         if (grasses == null) return null;
         return grasses.stream()
                 .map(g -> "Grass")
